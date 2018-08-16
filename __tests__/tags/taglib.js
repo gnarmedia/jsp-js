3740ef18408ba04201b7d5d4a4dbc26f038fb2ed
Adam Murphy <adam.murphy@guitarcenter.com>
2018-08-16 16:50:06 -0700

    Replace Mocha with Jest, add tests for tagLib

---
+++ __tests__/tags/taglib.js
@@ -0,0 +1,57 @@
+const Renderer = require('../../lib/renderer');
+const TagLib = require('../../lib/tags/tagLib');
+
+const renderer = new Renderer({});
+const tagLib = new TagLib({}, renderer);
+
+describe('class TagLib', () => {
+    describe('cleanCodeExp()', () => {
+        it('should clear the ${} from a code expression', () => {
+            const codeExpText = '${success && otherSuccess || otherValue > 5}';
+            const cleanedText = 'success && otherSuccess || otherValue > 5';
+
+            expect(tagLib.cleanCodeExpText(codeExpText)).toEqual(cleanedText);
+        });
+        it('should return the already clean expression', () => {
+            const cleanedText = 'success && otherSuccess || otherValue > 5';
+
+            expect(tagLib.cleanCodeExpText(cleanedText)).toEqual(cleanedText);
+        });
+    });
+
+    describe('supports()', () => {
+        it('should return false for tag not in tagMap', () => {
+            expect(tagLib.supports('not found')).toEqual(false);
+        });
+        it('should return true for tag in tagMap', () => {
+            const mockedTagLib = tagLib;
+
+            mockedTagLib.tagMap = {
+                'test':   () => true
+            };
+
+            expect(mockedTagLib.supports('test')).toEqual(true);
+        });
+    });
+
+    describe('handle()', () => {
+        it('should throw an error when the tag is unhandled', () => {
+            expect(() => tagLib.handle('madeUpTag')).toThrow('Unhandled tag "madeUpTag"');
+        });
+        it('should handle the tag according to the tagMap', () => {
+            const mockedTagLib = tagLib;
+
+            mockedTagLib.tagMap = {
+                'test':   (...params) => params
+            };
+
+            expect(tagLib.handle('test', 'node', 'index', 'data')).toEqual(['node', 'index', 'data']);
+        });
+    });
+
+    describe('wrapReturn()', () => {
+        it('should wrap the returned content in an object', () => {
+            expect(tagLib.wrapReturn('content', 'data')).toEqual({content: 'content', data: 'data'});
+        });
+    });
+});
